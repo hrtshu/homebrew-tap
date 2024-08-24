53e2374f6c4c404547c0fb651c22c309f7fd0393
Shuhei <sh7916@gmail.com>
2024-08-24 12:11:48 +0900

    update WindowPlacer v0.2.0

--- Formula/window-placer.rb
+++ Formula/window-placer.rb
@@ -2,9 +2,9 @@
 class WindowPlacer < Formula
   desc "A tool to manage window placement"
   homepage "https://github.com/hrtshu/WindowPlacer"
-  url "https://github.com/hrtshu/WindowPlacer/releases/download/v0.1.0/WindowPlacer"
-  sha256 "e7b72ce480fe561cfd3fccda537aa206f5af126631d19db7e7a462f669f3ddd2"
-  version "0.1.0"
+  url "https://github.com/hrtshu/WindowPlacer/releases/download/v0.2.0/WindowPlacer"
+  sha256 "f7509d8c958cca9be0a5812a234e898b08a94b2ee209be74f1ea6aa28bef782b"
+  version "0.2.0"
 
   def install
     bin.install "WindowPlacer"
