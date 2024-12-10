121b25feb46e563e675d8b22b427234a09772900
Shuhei <sh7916@gmail.com>
2024-12-11 00:04:34 +0900

    update FocusSidebar to v0.3.0

--- Casks/focus-sidebar.rb
+++ Casks/focus-sidebar.rb
@@ -1,15 +1,14 @@
 cask "focus-sidebar" do
   name "FocusSidebar"
-  version "0.2.0"
+  version "0.3.0"
 
   arch arm: "arm64-mac", intel: "mac"
 
-  url "https://github.com/hrtshu/FocusSidebarReleases/releases/download/v#{version}/FocusSidebar-0.2.0-#{arch}.zip"
+  url "https://github.com/hrtshu/FocusSidebarReleases/releases/download/v#{version}/FocusSidebar-#{version}-#{arch}.zip"
   desc "FocusSidebar"
   homepage "https://github.com/hrtshu/FocusSidebar"
 
-  sha256 arm: "ef9a36e5bca7689f089d572c7cab495b97dc9a61e6aec4eb6b6626ff107084fc",
-         intel: "cc0231f914af57ec02e23759d84460d21e15362474dafeace4857090dd5d9925"
+  sha256 :no_check
 
   app "FocusSidebar.app"
 
